95899cdc5638ace78f22018562ce95f53348bc78
Micaelabc <micaela.mbc@hotmail.com>
2017-06-26 12:35:07 -0300

    Clase 1 Programacion

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,10 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
-{
+{var nombre;
+nombre = prompt();
+alert (nombre);
+
 	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -3,7 +3,16 @@
 	y lo muestro por 'getElementById' al presionar el botón 'MOSTRAR'
 */
 function Mostar()
-{
+{ var nombre;
+	nombre = prompt()
+	document.getElementById ("elNombre").value; // es al reves, le asignas la variable
+	document.getElementById ("elNombre").value; = nombre;
+	
+
+
+	
+
+
 	
 }
 
