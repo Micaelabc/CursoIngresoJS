5750b835314d5368946984896b19d5489e639562
Micaelabc <micaela.mbc@hotmail.com>
2017-06-28 01:38:44 -0300

    Update jsEntradaSalida-6.js

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -2,7 +2,13 @@
 transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
-{
+{var numeroUno = document.getElementById ("numeroUno"). value;
+ var numeroDos = document.getElementById ("numeroDos"). value;
+  //parseInt lo usamos para convertir los numeros que lee como texto, para poder hacer las cuentas
+var numeroConvertidoUno = parseInt(numeroUno);
+var numeroConvertidoDos = parseInt(numeroDos);
+alert(numeroConvertidoUno + numeroConvertidoDos);
+
 
 }
 
